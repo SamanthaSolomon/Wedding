f46ca8ce5d16c90a35a774a29d761135a54980e4
Samantha <sleahsolomon@gmail.com>
2024-02-25 17:12:30 -0500

    add colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSee_6A2bktpfFY5MDVvCrXvlRFmGl18ATalCLTvs-G2CT7lpw/viewform?embedded=true" width="640" height="1251" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSee_6A2bktpfFY5MDVvCrXvlRFmGl18ATalCLTvs-G2CT7lpw/viewform?embedded=true" width="640" height="1251">Loading…</iframe>
       </div>
     </main>
   );
 }
+
+
+//https://docs.google.com/forms/d/139-OC-rS0JWLOR3JoMXxDzFx-nz5sUFjY8jEKdXasHA/edit
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'stone': {
+        200: '#e7e5e4',
+        500: '#78716c',
+        700: '#44403c',
+        900: '#1c1917',
+        950: '#0c0a09'
+      },
+      'amber' : '#92400e',
+      'emerald' :'#022c22',
+      'rose' :'#4c0519'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
