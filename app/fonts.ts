55b4ac2371f99cf0f1c17ba0b84fa91b984bda92
Samantha <sleahsolomon@gmail.com>
2024-03-03 13:10:51 -0500

    finish std version

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,14 @@
-import { Monsieur_La_Doulaise } from 'next/font/google'
+import { Meddon } from 'next/font/google'
+import { Abhaya_Libre } from 'next/font/google'
 
-export const monsieur_la_doulaise = Monsieur_La_Doulaise({
+export const meddon = Meddon({
   subsets: ['latin'],
   display: 'swap',
-  weight: '400'
+  weight: '400',
+})
+
+export const abhaya_libre = Abhaya_Libre({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
 })
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { monsieur_la_doulaise } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Chuck and Samantha's Wedding Website",
@@ -14,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={monsieur_la_doulaise.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
