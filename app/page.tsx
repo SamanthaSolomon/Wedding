cefcc09fcc83416125aad5bd63947390bbf27404
Samantha <sleahsolomon@gmail.com>
2024-03-03 13:22:17 -0500

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,16 @@ export default function Home() {
   <main className="flex justify-center min-h-screen items-center p-7 xl:p-24 bg-gradient-to-r from-amber to-stone-200">
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-14 content-around">
       <div className="flex flex-col justify-center">
-        <Image src="/std.png" alt="save the date" width={500} height={500}></Image>
+        <Image src="/std.png" alt="save the date" width={545} height={500}></Image>
         {/* <h1 className={`${meddon.className} xl:text-5xl text-stone-950 leading-loose`}>Wedding and Handfasting of Samantha and Chuck</h1> */}
       </div>
       <div className="flex flex-col justify-center"> 
-        <div className="button">
-          <Link target="blank" href="https://forms.gle/VEKxyEejELfjuyE78">
+          <Link target="blank" href="https://forms.gle/VEKxyEejELfjuyE78" className="button">
             <button className={`${abhaya_libre.className} text-xl`}>Survey</button>
           </Link>
-        </div>
-        <div className="button mt-5 lg:mt-10">
-          <Link target="blank" href="https://mimslyninn.com/">
+        <Link target="blank" href="https://mimslyninn.com/luray-hotel-accommodations/" className="button mt-5 lg:mt-10">
             <button className={`${abhaya_libre.className} text-xl`}>Book Your Room</button>
-          </Link>  
-        </div>
+        </Link>  
         <p className={`${abhaya_libre.className} text-base mt-4`}>Less expensive rooms in group house available. Contact Samantha or Chuck for info.</p>
       </div>
     </div>
