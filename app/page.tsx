9d210075b228741890de697593f58ea16b7a6363
SamanthaSolomon <samantha.solomon@leesa.com>
2024-09-22 12:20:32 -0400

    attire

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,24 @@ export default function Home() {
         <Image src="/invite-front.jpeg" alt="invite front" width={545} height={500}></Image>
       </div>
       <div className="flex flex-col justify-center  xl:max-w-[500px]"> 
-          <Link target="blank" href="#" className="button">
+          <Link href="#lodging" className="button">
             <button className={`${meddon.className} text-2xl`}>Lodging</button>
           </Link>
-          <Link target="blank" href="#" className="button mt-5 lg:mt-10">
+          <Link href="#ceremony" className="button mt-5 lg:mt-10">
             <button className={`${meddon.className} text-2xl`}>Ceremony & Reception</button>
           </Link>
-        <Link target="blank" href="#" className="button mt-5 lg:mt-10">
+        <Link href="#schedule" className="button mt-5 lg:mt-10">
             <button className={`${meddon.className} text-2xl`}>Order of the Weekend</button>
         </Link>
-        <Link target="blank" href="#" className="button mt-5 lg:mt-10">
+        <Link target="blank" href="https://registry.theknot.com/chuck-dye-samantha-solomon-january-2025-md/67476230" className="button mt-5 lg:mt-10">
             <button className={`${meddon.className} text-2xl`}>Gift Registry</button>
         </Link>
-        <Link target="blank" href="#" className="button mt-5 lg:mt-10">
+        <Link href="#attire" className="button mt-5 lg:mt-10">
             <button className={`${meddon.className} text-2xl`}>Attire Inspiration</button>
         </Link>   
       </div>
       {/* lodging */}
-      <div className="flex flex-col justify-center xl:max-w-[500px] my-40">
+      <div id='lodging' className="flex flex-col justify-center xl:max-w-[500px] my-40">
         <h1 className={`${meddon.className} text-base xl:text-6xl my-8`}>Lodging</h1>
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}> The Mimslyn Inn<br></br>401 W. Main St.<br></br>Luray, VA 22835-5260</p>
         <p className={`${abhaya_libre.className} font-bold text-base xl:text-xl mt-4`}>Please call (540) 743-5105 and specify you are a Dye-Solomon Wedding Guest to get our event rate.</p>
@@ -39,7 +39,7 @@ export default function Home() {
         <Image src="/mimslyn-color.jpg" alt="mimslyn inn" width={545} height={500}></Image>
       </div>
       {/* Ceremony & Reception */}
-      <div className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
+      <div id='ceremony' className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
         <Image src="/Luray-Vertical.jpg" alt="luray" width={545} height={500}></Image>
       </div>
       <div className="flex flex-col justify-center xl:max-w-[500px] my-40">
@@ -50,7 +50,7 @@ export default function Home() {
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>Reception to follow at The Mimslyn Inn.</p>
       </div>
       {/* Order of the Weekend */}
-      <div className="flex flex-col justify-center xl:max-w-[500px] my-40">
+      <div id='schedule' className="flex flex-col justify-center xl:max-w-[500px] my-40">
         <h1 className={`${meddon.className} text-base xl:text-6xl my-8`}>Order of the Weekend</h1>
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4 underline underline-offset-8`}>Saturday, January 18th</p>
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>3:00pm<br></br>Check In at The Mimslyn Inn</p>
@@ -65,6 +65,14 @@ export default function Home() {
       </div>
       <div className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
         <Image src="/tarot.jpg" alt="tarot" width={545} height={500}></Image>
+      </div>
+      {/* attire inspiration */}
+      <div className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
+        <Image src="/creeper.jpg" alt="creeper lady" width={545} height={500}></Image>
+      </div>
+      <div id='attire' className="flex flex-col justify-center xl:max-w-[500px] my-40">
+        <h1 className={`${meddon.className} text-base xl:text-6xl my-8`}>Attire Inspriation</h1>
+        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>Formal wedding attire sits between cocktail and black tie on the wedding dress code scale. It's slightly more elevated than cocktail attire and a touch less dressy than a black-tie nuptial. We encourage guests to lean into our theme of wintery woods, pagan, witchy, and ritual.</p>
       </div>
     </div>
   </main>
