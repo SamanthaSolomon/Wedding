4e75d57bc9def1d67d7f4d1770f948072303ed89
Samantha <sleahsolomon@gmail.com>
2024-09-21 22:49:43 -0400

    new images

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,16 @@ export default function Home() {
   <main className="flex justify-center min-h-screen items-center p-7 xl:p-24 bg-gradient-to-r from-amber to-stone-200">
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-14 justify-items-center ">
       <div className="flex flex-col justify-center">
-        <Image src="/std.png" alt="save the date" width={545} height={500}></Image>
-        {/* <h1 className={`${meddon.className} xl:text-5xl text-stone-950 leading-loose`}>Wedding and Handfasting of Samantha and Chuck</h1> */}
+        <Image src="/public/invite-front.jpeg" alt="invite front" width={545} height={500}></Image>
       </div>
       <div className="flex flex-col justify-center  xl:max-w-[500px]"> 
           <Link target="blank" href="https://forms.gle/VEKxyEejELfjuyE78" className="button">
             <button className={`${abhaya_libre.className} text-xl`}>Survey</button>
           </Link>
         <Link target="blank" href="https://mimslyninn.com/luray-hotel-accommodations/" className="button mt-5 lg:mt-10">
-            <button className={`${abhaya_libre.className} text-xl`}>Book Your Room</button>
+            <button className={`${abhaya_libre.className} text-xl`}>Accommodations</button>
         </Link>  
-        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>This will be a weekend event at The Mimslyn Inn in Luray, VA with ceremony and reception on Saturday and breakfast on Sunday. Guests are welcome to arrive on Friday or Saturday.</p> 
+        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>Hello help  This will be a weekend event at The Mimslyn Inn in Luray, VA with ceremony and reception on Saturday and breakfast on Sunday. Guests are welcome to arrive on Friday or Saturday.</p> 
         <p className={`${abhaya_libre.className} font-bold text-base xl:text-xl mt-4`}>Please call (540) 743-5105 and specify you are a Dye-Solomon Wedding Guest to get our event rate.</p>
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}> Less expensive rooms in group house available. Contact Samantha or Chuck for info.</p>
       </div>
@@ -28,4 +27,3 @@ export default function Home() {
 }
 
 
-//https://docs.google.com/forms/d/139-OC-rS0JWLOR3JoMXxDzFx-nz5sUFjY8jEKdXasHA/edit
