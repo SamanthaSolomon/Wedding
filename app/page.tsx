6326125c8f210d252bf6af9194af3f608441699d
SamanthaSolomon <samantha.solomon@leesa.com>
2024-09-22 11:35:17 -0400

    ceremony and reception section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
   <main className="flex justify-center min-h-screen items-center p-7 xl:p-24 bg-gradient-to-r from-stone-700 to-stone-200">
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-14 justify-items-center">
+      {/* navigation */}
       <div className="flex flex-col justify-center">
         <Image src="/invite-front.jpeg" alt="invite front" width={545} height={500}></Image>
       </div>
@@ -27,6 +28,7 @@ export default function Home() {
             <button className={`${meddon.className} text-2xl`}>Attire Inspiration</button>
         </Link>   
       </div>
+      {/* lodging */}
       <div className="flex flex-col justify-center xl:max-w-[500px] my-40">
         <h1 className={`${meddon.className} text-base xl:text-6xl my-8`}>Lodging</h1>
         <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}> The Mimslyn Inn<br></br>401 W. Main St.<br></br>Luray, VA 22835-5260</p>
@@ -35,6 +37,16 @@ export default function Home() {
       </div>
       <div className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
         <Image src="/mimslyn-color.jpg" alt="invite front" width={545} height={500}></Image>
+      </div>
+      {/* Ceremony & Reception */}
+      <div className="flex flex-col justify-center  xl:max-w-[500px] mt-8">
+        <Image src="/Luray-Vertical.jpg" alt="invite front" width={545} height={500}></Image>
+      </div>
+      <div className="flex flex-col justify-center xl:max-w-[500px] my-40">
+        <h1 className={`${meddon.className} text-base xl:text-6xl my-8`}>Ceremony & Reception</h1>
+        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>Luray Caverns<br></br>101 Cave Hill Rd.<br></br>Luray, VA 22835-5260</p>
+        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}>Ceremony include a 1.75 mile guided walking tour through the caverns. The path is well lit with small inclines.</p>
+        <p className={`${abhaya_libre.className} text-base xl:text-xl mt-4`}> A shortcut directly to the ceremony site is available. Please get in touch with Chuck or Sam if interested.</p>
       </div>
     </div>
   </main>
